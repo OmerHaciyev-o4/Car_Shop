5914cd96ab9e1a7271978fe914c2169888efd2ba
OmerHaciyev-o4 <omer04118102@gmail.com>
2022-01-15 22:58:01 +0400

    Car Shop

---
+++ Car_Shop/MVVM/View/LoginScreen.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Car_Shop.MVVM.View
+{
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseMove(object sender, MouseEventArgs e)
+        {
+            xbtn.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            xbtn.Background = new SolidColorBrush(Colors.Transparent);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void xbtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
